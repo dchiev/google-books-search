2f745ea4e37ecec174c58b526994fd6b45390c0a
Dan Chiev <danchiev2@Gmail.com>
2021-02-04 23:24:46 -0500

    added react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,53 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Saved from "./components/Saved";
+import Search from "./components/Search";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Google Books Search</Link>
+            </li>
+            <li>
+              <Link to="/saved">Saved</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/saved">
+            <Saved />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </Router>
   );
 }
 
+function Home() {
+  return <h2>Home</h2>;
+}
 
-export default App;
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Booksearch() {
+function Search() {
   return <div></div>;
 }
 
-export default Booksearch;
+export default Search;
